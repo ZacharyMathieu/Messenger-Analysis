eab696a41ac6652df1705d7d7e5c0817213153b6
zac <zacmathieu1@gmail.com>
2023-10-13 13:16:10 -0400

    typing

--- FileReader.py
+++ FileReader.py
@@ -3,7 +3,7 @@ import json
 import Constants
 
 
-def read(message_file_path) -> dict[str, any]:
+def read(message_file_path: str) -> dict[str, any]:
     f = open("./{}/{}".format(Constants.FILES_FOLDER_PATH, message_file_path), "r", encoding="utf-8")
     data = json.load(f)
 
